2f1498c0d69bc474ac147f23f7e296159f90acde
Amon Kiptoo <akiptoo@mtechcomm.co.ke>
2021-11-01 09:35:02 +0300

    updated on the test cases under com.amon.blog.springblog

--- src/test/java/com/amon/blog/springblog/SpringblogApplicationTests.java
+++ src/test/java/com/amon/blog/springblog/SpringblogApplicationTests.java
@@ -4,20 +4,24 @@ import com.amon.blog.springblog.model.Post;
 import com.amon.blog.springblog.model.User;
 import com.amon.blog.springblog.repository.PostRepository;
 import com.amon.blog.springblog.repository.UserRepository;
+import java.util.List;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringblogApplicationTests {
-    
+
     UserRepository userRepo;
     PostRepository postrepo;
-    
+
     @Test
     void contextLoads() {
     }
-    
+
     @Test
     public void userCreate() {
         User user = new User();
@@ -26,9 +30,9 @@ class SpringblogApplicationTests {
         user.setUsername("testUsername");
         userRepo.save(user);
         Assertions.assertNotNull(userRepo.findByUsername("testUsername"));
-        
+
     }
-    
+
     @Test
     public void postCreate() {
         Post post = new Post();
@@ -39,7 +43,17 @@ class SpringblogApplicationTests {
         post.setUpdatedOn(new java.util.Date());
         postrepo.save(post);
         Assertions.assertNotNull(postrepo.findById(Long.valueOf(1)));
-        
+
     }
-    
+
+    public void testReadAllPosts() {
+        List<Post> posts = postrepo.findAll();
+        assertThat(posts, not(IsEmptyCollection.empty()));
+    }
+
+    public void testReadAllUsers() {
+        List<User> users = userRepo.findAll();
+        assertThat(users, not(IsEmptyCollection.empty()));
+    }
+
 }
