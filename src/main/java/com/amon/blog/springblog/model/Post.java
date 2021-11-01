62ca91b519e95c5996655ed05f5e442d588e2938
Amon Kiptoo <akiptoo@mtechcomm.co.ke>
2021-11-01 08:26:06 +0300

    updated on the test cases under com.amon.blog.springblog

--- src/main/java/com/amon/blog/springblog/model/Post.java
+++ src/main/java/com/amon/blog/springblog/model/Post.java
@@ -152,5 +152,9 @@ public class Post {
     public void setContent(String content) {
         this.content = content;
     }
+
+    public void setId(long l) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- src/test/java/com/amon/blog/springblog/SpringblogApplicationTests.java
+++ src/test/java/com/amon/blog/springblog/SpringblogApplicationTests.java
@@ -1,13 +1,45 @@
 package com.amon.blog.springblog;
 
+import com.amon.blog.springblog.model.Post;
+import com.amon.blog.springblog.model.User;
+import com.amon.blog.springblog.repository.PostRepository;
+import com.amon.blog.springblog.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringblogApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    
+    UserRepository userRepo;
+    PostRepository postrepo;
+    
+    @Test
+    void contextLoads() {
+    }
+    
+    @Test
+    public void userCreate() {
+        User user = new User();
+        user.setEmail("testemail@email.com");
+        user.setPassword("password");
+        user.setUsername("testUsername");
+        userRepo.save(user);
+        Assertions.assertNotNull(userRepo.findByUsername("testUsername"));
+        
+    }
+    
+    @Test
+    public void postCreate() {
+        Post post = new Post();
+        post.setContent("test content");
+        post.setId(1);
+        post.setTitle("test title");
+        post.setCreatedOn(new java.util.Date());
+        post.setUpdatedOn(new java.util.Date());
+        postrepo.save(post);
+        Assertions.assertNotNull(postrepo.findById(Long.valueOf(1)));
+        
+    }
+    
 }
